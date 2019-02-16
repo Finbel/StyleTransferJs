e038d2df362ba210938448ed9484c60a78588863
Birger Moëll <birger.moell@gmail.com>
2019-02-16 23:01:26 +0100

    Added files from image project

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,180 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { css } from "@emotion/core";
+import { ClipLoader } from "react-spinners";
+
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef(); // Create a ref object
+    this.state = { showPrediction: false, loading: false, pictures: {} };
+  }
+
+  handleUpload = async () => {
+    console.log("uploaded the image");
+
+    this.setState({
+      loading: true
+    });
+    setTimeout(this.upload, 750);
+  };
+
+  reqListener = () => {
+    console.log(this.responseText);
+  };
+
+  base64encode = () => {
+    return new Promise((resolve, reject) => {
+      var file = this.state.selectedFile;
+      var reader = new FileReader();
+      reader.onloadend = () => {
+        console.log("RESULT", reader.result);
+        resolve(reader.result)
+      }
+      reader.readAsDataURL(file);
+    })
+  };
+
+  decode_utf8 = (s) => {
+    console.log("inside decode with", s)
+    return new Promise((resolve, reject) => {
+      resolve(decodeURIComponent(escape(s)))
+    })
+  }
+
+
+  upload = async () => {
+    this.setState({
+      showPrediction: true,
+      loading: false
+    });
+
+    let file = this.state.selectedFile;
+    console.log("the file is", file);
+    let base64img = await this.base64encode();
+    console.log("the image is", base64img)
+
+    let diagnosis
+
+    if (this.state.diagnosis) {
+      diagnosis = this.state.diagnosis;
+    } else {
+      diagnosis = "Pneumonia"
+    }
+
+    let url = "http://localhost:5000/uploader";
+    try {
+      let data = { base64img, diagnosis };
+      // try to send the image using fetch
+      let response = await fetch(url, {
+        method: "POST", // or 'PUT'
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      console.log("the response is", response)
+
+      let responseJson = await response.json();
+      console.log("the response json is", responseJson);
+      const imageString = responseJson.encodedimage
+      const decodedImage = await this.decode_utf8(imageString)
+
+      this.setState({
+        response: responseJson,
+        image: decodedImage
+      })
+    } catch (err) {
+      console.error("Error:", err);
+    }
+
+    // var formData = new FormData();
+
+
+
+    // // HTML file input, chosen by user
+    // formData.append("file", file);
+    // var request = new XMLHttpRequest();
+    // request.addEventListener("load", this.reqListener);
+    // request.open("POST", "http://localhost:5000/uploader");
+    // request.send(formData);
+  };
+
+  handleselectedFile = event => {
+    this.setState({
+      selectedFile: event.target.files[0]
+    });
+  };
+
+  setDiagnosis = event => {
+    this.setState({ diagnosis: event.target.value });
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App-header">
+          {this.state.selectedFile && (
+            <div>
+              <img
+                className="App-logo"
+                src={URL.createObjectURL(this.state.selectedFile)}
+              />
+            </div>
+          )}
+
+          <p>Style transfer</p>
+          <div>
+            {/* <select onChange={this.setDiagnosis} value={this.state.value}>
+              <option value="Atelectasis">Atelectasis</option>
+              <option value="Cardiomegaly">Cardiomegaly</option>
+              <option value="Effusion">Effusion</option>
+              <option value="Infiltration">Infiltration</option>
+              <option value="Mass">Mass</option>
+              <option value="Nodule">Nodule</option>
+              <option selected value="Pneumonia">
+                Pneumonia
+              </option>
+              <option value="Pneumothorax">Pneumothorax</option>
+              <option value="Consolidation">Consolidation</option>
+              <option value="Edema">Edema</option>
+              <option value="Emphysema">Emphysema</option>
+              <option value="Fibrosis">Fibrosis</option>
+              <option value="Pleural_Thickening">Pleural_Thickening</option>
+              <option value="Hernia">Hernia</option>
+            </select> */}
+            <input type="file" name="file" onChange={this.handleselectedFile} />
+            <button onClick={this.handleUpload}>Style transfer</button>
+          </div>
+
+          <div className="Prediction-container">
+            <div className="Prediction-loader-container">
+              <ClipLoader
+                css={override}
+                sizeUnit={"px"}
+                size={150}
+                color={"#123abc"}
+                loading={this.state.loading}
+              />
+            </div>
+          </div>
+
+          {this.state.showPrediction && this.state.response && this.state.response.prediction && (
+            <div className="Prediction-container" id="prediction">
+              <h5>Prediction, Pneumonia, {this.state.response.prediction["Predicted Probability"]["Pneumonia"]}</h5>
+              {/* <img className="Prediction-image" src={this.state.response.encodedimage}/> */}
+            </div>
+          )}
+
+        </div>
       </div>
     );
   }
